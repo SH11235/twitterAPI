d609e75a82b3c55b83ca51e1a32286f526921ba7
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2020-10-14 18:19:27 +0900

    add AWSにssh後に実行するスクリプトファイル

---
+++ aws_init.sh
@@ -0,0 +1,12 @@
+sudo yum update
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+cat /etc/group | grep docker
+
+sudo docker build -t python_cgi . --no-cache
+#aws セキュリティグループ項目で8000ポートを開ける
+sudo docker run -d -p 8000:8000 python_cgi
+
+# 止めるとき
+# sudo docker kill $(sudo docker ps -q)
