9650552373e7718d1793d45a26b9162939c5ca5a
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2020-12-07 11:51:53 +0900

    refactor AWSにファイルをリリースするにあたって、ディレクトリやshellを修正

--- aws_init.sh
+++ aws_init.sh
@@ -2,9 +2,10 @@ sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
-cat /etc/group | grep docker
-
-sudo docker build -t python_cgi . --no-cache
+mkdir cgi-bin
+mv search.py cgi-bin
+mv config.py cgi-bin
+sudo docker build -t python_cgi .
 #aws セキュリティグループ項目で8000ポートを開ける
 sudo docker run -d -p 8000:8000 python_cgi
 
